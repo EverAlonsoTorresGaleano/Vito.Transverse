f62442528de1c617eb8cd41fa0f9a4bfc07d0942
Ever Alonso Torres Galeano <eeatg844@hotmail.com>
2025-04-29 14:03:22 -0500

    Enable Docker

--- Vito.Transverse.Identity.DAL/TransverseRepositories/Localization/LocalizationRepository.cs
+++ Vito.Transverse.Identity.DAL/TransverseRepositories/Localization/LocalizationRepository.cs
@@ -43,6 +43,7 @@ public class LocalizationRepository(IDataBaseContextFactory _dataBaseContextFact
         catch (Exception ex)
         {
             _logger.LogError(ex, message: nameof(GetAllLocalizedMessagesByCultureIdAsync));
+            throw;
             returnList = [];// MockDataPopulateEntity.CreateMockDataLocalization().Where(x => x.CultureFk.Equals(cultureId)).ToList();
         }
         finally
